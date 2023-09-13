4d0e7b2e829dbcc22e588d1e117a4aa8d1e7ed9e
Juan Ignacio Pintos Elso <35276175+Juanpintoselso33@users.noreply.github.com>
2023-09-13 19:04:42 +0000

    ULTIMO COMMIT DE LA BRANCH

---
+++ src/front/js/pages/addProduct.js
@@ -0,0 +1,120 @@
+import React, { useState, useContext } from 'react';
+import { Link } from "react-router-dom";
+import { Context } from '../store/appContext';  // Asegúrate de importar tu Contexto correctamente
+
+export const AddProduct = () => {
+    const { actions } = useContext(Context);
+    const [productData, setProductData] = useState({
+        cost: '',
+        name: '',
+        description: '',
+        stars: '',
+        img_url: '',
+        category: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProductData({
+            ...productData,
+            [name]: value
+        });
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setProductData({
+                ...productData,
+                img_url: reader.result
+            });
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Verificar que todos los campos requeridos estén llenos
+        if (!productData.cost || !productData.name || !productData.description || !productData.category) {
+            alert("Por favor, completa todos los campos requeridos.");
+            return;
+        }
+
+        // Verificar que al menos una de las dos opciones de imagen esté llena
+        if (!productData.img_url && !productData.img_file) {
+            alert("Por favor, sube una imagen o proporciona una URL de imagen.");
+            return;
+        }
+
+        // Si ambas opciones de imagen están llenas, dar prioridad a la imagen subida localmente
+        if (productData.img_file) {
+            productData.img_url = productData.img_file;
+        }
+
+        actions.addProduct(productData);
+    };
+
+    return (
+        <section className="vh-100 bg-image"
+            style={{ backgroundImage: "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')" }}>
+            <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+                <div className="container h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-12 col-md-9 col-lg-9 col-xl-6">
+                            <div className="card" style={{ borderRadius: '15px' }}>
+                                <div className="card-body p-5">
+                                    <h2 className="text-uppercase text-center mb-5">Add a Product</h2>
+                                    <form onSubmit={handleSubmit}>
+                                        <div className="form-outline mb-4">
+                                            <input type="number" name="cost" placeholder="Cost" onChange={handleChange} required className="form-control form-control-lg" />
+                                            <label className="form-label">Cost</label>
+                                        </div>
+                                        <div className="form-outline mb-4">
+                                            <input type="text" name="name" placeholder="Name" onChange={handleChange} required className="form-control form-control-lg" />
+                                            <label className="form-label">Name</label>
+                                        </div>
+                                        <div className="form-outline mb-4">
+                                            <input type="text" name="description" placeholder="Description" onChange={handleChange} required className="form-control form-control-lg" />
+                                            <label className="form-label">Description</label>
+                                        </div>
+                                        <div className="form-outline mb-4">
+                                            <input type="number" name="stars" placeholder="Stars" onChange={handleChange} className="form-control form-control-lg" />
+                                            <label className="form-label">Stars</label>
+                                        </div>
+                                        <div className="form-outline mb-4">
+                                            <input type="file" name="img_file" onChange={handleFileChange} className="form-control form-control-lg" />
+                                            <label className="form-label">Upload Image</label>
+                                        </div>
+                                        <div className="form-outline mb-4">
+                                            <input type="text" name="img_url" placeholder="Or Image URL" onChange={handleChange} className="form-control form-control-lg" />
+                                            <label className="form-label">Or Image URL</label>
+                                        </div>
+                                        <div className="form-outline mb-4">
+                                            <select name="category" onChange={handleChange} required className="form-control form-control-lg">
+                                                <option value="">Select Category</option>
+                                                <option value="H">Hamburguesas</option>
+                                                <option value="M">Milanesas</option>
+                                            </select>
+                                            <label className="form-label">Category</label>
+                                        </div>
+                                        <div className="d-flex justify-content-center">
+                                            <button type="submit" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Add Product</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AddProduct;
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,17 +1,20 @@
+import React from 'react';
+import axios from 'axios';
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-/* ----------<productos>--------------- */
-			
-
-
-			
-productos: [],
+			/* ----------<productos>--------------- */
 
 
 
 
-/* ----------</productos>--------------- */
+			productos: [],
+
+
+
+
+			/* ----------</productos>--------------- */
 
 
 
@@ -25,7 +28,7 @@ productos: [],
 			message: null,
 
 			planetas: [],
-			
+
 			demo: [
 				{
 					title: "FIRST",
@@ -44,38 +47,38 @@ productos: [],
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			login: async (email, password)=> {
+			login: async (email, password) => {
 				console.log("fnciona")
-			 	try {
-			 		let data = await axios.post('https://fantastic-couscous-97994xr7px55hjvp-3000.app.github.dev/login',{
-			 				"email":email,
-			 				"password":password
-			 			})
-			 		console.log(data);
+				try {
+					let data = await axios.post('https://animated-telegram-54x4vxwwxq6hpgg9-3001.app.github.dev/api/login', {
+						"email": email,
+						"password": password
+					})
+					console.log(data);
 					//Guardar en el navegador el token
-					localStorage.setItem("token",data.data.access_token);
+					localStorage.setItem("token", data.data.access_token);
 					setStore({ isAuthenticated: true }); // Actualiza el estado a true
-  
+
 					return true;
-			 		}
-			 		catch(error){
-			 			if (error.response.status === 404){
-							alert(error.response.data.msg)
-						}
-						
-			 		}
-			
-				},
+				}
+				catch (error) {
+					if (error.response.status === 404) {
+						alert(error.response.data.msg)
+					}
+
+				}
+
+			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
@@ -94,23 +97,60 @@ productos: [],
 				setStore({ demo: demo });
 			},
 
-			
-/* -----------------Productos-----------------*/
+
+			/* -----------------Productos-----------------*/
 			obtenerAllProducts: async function () {
 				try {
-					let response = await fetch("https://cautious-space-waffle-v6vw5vw54j73w7q6-3001.app.github.dev/api/products");
-					let data = await response.json();  
-					setStore({ productos: data }); 	
-					
-				} 
-				
+					let response = await fetch("https://animated-telegram-54x4vxwwxq6hpgg9-3001.app.github.dev/api/products");
+					let data = await response.json();
+					setStore({ productos: data });
+
+				}
+
 				catch (error) {
 					console.log(error);
 
 				}
 			},
-/* -----------------Productos-----------------*/
+			/* -----------------Registro-----------------*/
 
+			//Funcion para dar de alta registros
+			registerUser: async (formData) => {
+				try {
+				  const response = await axios.post('https://animated-telegram-54x4vxwwxq6hpgg9-3001.app.github.dev/api/register', formData);
+			  
+				  if (response.status === 200 || response.status === 201) {
+					console.log('Registro exitoso:', response.data);
+					// Aquí puedes actualizar el estado o hacer otras acciones
+				  } else {
+					console.log('Error en el registro:', response);
+					console.log('Estado de la respuesta:', response.status);
+					console.log('Cuerpo de la respuesta:', response.data);
+			  
+					if (response.data.error) {
+					  alert(response.data.error); // Mostrar el mensaje de error al usuario
+					}
+				  }
+				} catch (error) {
+				  console.error('Hubo un problema con la petición:', error);
+				  if (error.response && error.response.data && error.response.data.error) {
+					alert(error.response.data.error); // Mostrar el mensaje de error al usuario
+				  }
+				}
+			  },
+			  
+			  addProduct: async (productData) => {
+				try {
+				  const response = await axios.post('https://animated-telegram-54x4vxwwxq6hpgg9-3001.app.github.dev/api/products', productData);
+				  if (response.status === 200 || response.status === 201) {
+					console.log('Producto agregado exitosamente:', response.data);					
+				  } else {
+					console.log('Error al agregar el producto:', response);
+				  }
+				} catch (error) {
+				  console.error('Hubo un problema con la petición:', error);
+				}
+			  }
 
 
 
@@ -147,7 +187,6 @@ productos: [],
 
 
 
-			
 
 
 
@@ -209,7 +248,8 @@ productos: [],
 
 
 
-		}
+
+		} //FIN DE ACTIONS
 	};
 };
 
