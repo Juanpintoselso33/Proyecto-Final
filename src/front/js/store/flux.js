3a1a8d0acda8873f1cd4321fd8f99c8d74721aad
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-09-13 14:56:12 +0000

    Nuevos elementos Sec

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,8 +1,27 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+/* ----------<productos>--------------- */
+			
 
-			productos: [],
+
+			
+productos: [],
+
+
+
+
+/* ----------</productos>--------------- */
+
+
+
+
+
+
+
+
+
+
 			message: null,
 
 			planetas: [],
@@ -25,7 +44,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-
 			getMessage: async () => {
 				try{
 					// fetching data from the backend
@@ -53,8 +71,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({ demo: demo });
 			},
 
-
-			/* Productos */
+			
+/* -----------------Productos-----------------*/
 			obtenerAllProducts: async function () {
 				try {
 					let response = await fetch("https://cautious-space-waffle-v6vw5vw54j73w7q6-3001.app.github.dev/api/products");
@@ -68,8 +86,104 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				}
 			},
+/* -----------------Productos-----------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 		}
