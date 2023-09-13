d838b3b625893cbd5c63a49dcc44bfcddf6fba1d
Juan Pintos <juanpintoselso33@gmail.com>
2023-09-13 13:44:13 +0000

    Primer commit

---
+++ src/front/js/component/register.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+const RegisterModal = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+    // Aquí puedes manejar la lógica para enviar los datos del formulario
+    console.log('Form data submitted:', formData);
+  };
+
+  return (
+    <section className="vh-100 bg-image"
+      style={{ backgroundImage: "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')" }}>
+      <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+              <div className="card" style={{ borderRadius: '15px' }}>
+                <div className="card-body p-5">
+                  <h2 className="text-uppercase text-center mb-5">Create an account</h2>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="email"
+                        id="form3Example3cg"
+                        name="email"
+                        className="form-control form-control-lg"
+                        value={formData.email}
+                        onChange={handleChange}
+                      />
+                      <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="password"
+                        id="form3Example4cg"
+                        name="password"
+                        className="form-control form-control-lg"
+                        value={formData.password}
+                        onChange={handleChange}
+                      />
+                      <label className="form-label" htmlFor="form3Example4cg">Password</label>
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="password"
+                        id="form3Example4cdg"
+                        name="confirmPassword"
+                        className="form-control form-control-lg"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                      />
+                      <label className="form-label" htmlFor="form3Example4cdg">Confirm Password</label>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <button type="submit" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                    </div>
+                    <p className="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" className="fw-bold text-body"><u>Login here</u></a></p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RegisterModal;
