25d177a8f3f6bbceace0e4a34e6348ce8a3fd853
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-09-29 04:07:57 +0000

    responsiv detalle de producto

--- src/front/js/component/Extras_Productos.jsx
+++ src/front/js/component/Extras_Productos.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import 'react-multi-carousel/lib/styles.css';
 import "../../styles/Stylecard.css";
+import "../../styles/modal.css"
 import { useNavigate } from "react-router-dom";
 
 export const Extras_prod = () => {
@@ -91,20 +92,20 @@ export const Extras_prod = () => {
             <div className="modal fade pruebahome" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog pepesq1">
                     <div className="modal-content pruebasss">
-                        <div className="modal-header">
-                            <div className="row w-100">
-                                <div className="col-4">
-                                    <h1 className="modal-title fs-2 text-end" id="exampleModalLabel"> <strong> {dataDetalle.name}</strong></h1>
+                        <div className="modal-header header_Mod">
+                            <div className="row w-100 sub_header">
+                                <div className="nombre_modal">
+                                    <strong>{dataDetalle.name} </strong>
                                 </div>
-                                <div className="col-6">
-                                </div>
-                                <div className="col-2 text-end">
+                                {/* <div className="col-6">
+                                </div> */}
+                                <div className="col-2 text-end div_boton_x">
                                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                             </div>
                         </div>
                         <div className="modal-body contenedorImg_Data">
-                            <div className="row">
+                            <div className="row img_y_extras">
                                 <div className="col div_img_comida">
                                     <img className="imagenDetalle" src={dataDetalle.url} alt="" />
                                 </div>
@@ -157,43 +158,51 @@ export const Extras_prod = () => {
                         </div>
                         <div className="modal-footer pruebaas">
                             <div className="row w-100">
-                                <div className="col-3 text-end align-self-center">
-                                    <i
-                                        className="fas fa-minus text-secondary"
-                                        style={{
-                                            cursor: 'pointer',
-                                            fontSize: '2rem',
-                                            transition: 'font-size 0.3s ease'
-                                        }}
-                                        onMouseOver={(e) => e.target.style.fontSize = '2.2rem'}
-                                        onMouseOut={(e) => e.target.style.fontSize = '2rem'}
-                                        onMouseDown={(e) => e.target.style.fontSize = '2.4rem'}
-                                        onMouseUp={(e) => e.target.style.fontSize = '2rem'}
-                                        onClick={restarPro}
-                                    ></i>
-                                    <span
-                                        className="text-secondary align-self-center mx-2"
-                                        style={{ fontSize: '3rem' }}
-                                    >
-                                        {valor}
-                                    </span>
-                                    <i
-                                        className="fas fa-plus text-secondary"
-                                        style={{
-                                            cursor: 'pointer',
-                                            fontSize: '2rem',
-                                            transition: 'font-size 0.3s ease'
-                                        }}
-                                        onMouseOver={(e) => e.target.style.fontSize = '2.2rem'}
-                                        onMouseOut={(e) => e.target.style.fontSize = '2rem'}
-                                        onMouseDown={(e) => e.target.style.fontSize = '2.4rem'}
-                                        onMouseUp={(e) => e.target.style.fontSize = '2rem'}
-                                        onClick={sumarPro}
-                                    ></i>
+                                <div className="col-12 CN_P1">
+                                    <div className=" text-end align-self-center">
+                                        <i
+                                            className="fas fa-minus text-secondary"
+                                            style={{
+                                                cursor: 'pointer',
+                                                fontSize: '2rem',
+                                                transition: 'font-size 0.3s ease'
+                                            }}
+                                            onMouseOver={(e) => e.target.style.fontSize = '2.2rem'}
+                                            onMouseOut={(e) => e.target.style.fontSize = '2rem'}
+                                            onMouseDown={(e) => e.target.style.fontSize = '2.4rem'}
+                                            onMouseUp={(e) => e.target.style.fontSize = '2rem'}
+                                            onClick={restarPro}
+                                        ></i>
+                                        <span
+                                            className="text-secondary align-self-center mx-2"
+                                            style={{ fontSize: '3rem' }}
+                                        >
+                                            {valor}
+                                        </span>
+                                        <i
+                                            className="fas fa-plus text-secondary"
+                                            style={{
+                                                cursor: 'pointer',
+                                                fontSize: '2rem',
+                                                transition: 'font-size 0.3s ease'
+                                            }}
+                                            onMouseOver={(e) => e.target.style.fontSize = '2.2rem'}
+                                            onMouseOut={(e) => e.target.style.fontSize = '2rem'}
+                                            onMouseDown={(e) => e.target.style.fontSize = '2.4rem'}
+                                            onMouseUp={(e) => e.target.style.fontSize = '2rem'}
+                                            onClick={sumarPro}
+                                        ></i>
+                                    </div>
                                 </div>
-                                <div className="col-9 text-end">
-                                    <button type="button" className="btn btn-secondary me-2" data-bs-dismiss="modal" onClick={() => handleAddToCart(store.extrasSeleccionados)}>Agregar y seguir comprando</button>
-                                    <button type="button" className="btn btn-secondary" onClick={() => { handleAddToCart(store.extrasSeleccionados); mover_carrito(); }} data-bs-dismiss="modal">Agregar e ir a pagar</button>
+                                <div className="col-12 CN_P2">
+                                    {/* <div className="row text-end"> */}
+                                    <div className="c3po1">
+                                        <button type="button" className="btn btn-secondary me-2" data-bs-dismiss="modal" onClick={() => handleAddToCart(store.extrasSeleccionados)}>Agregar y seguir comprando</button>
+                                    </div>
+                                    <div className=" c3po2">
+                                        <button type="button" className="btn btn-secondary" onClick={() => { handleAddToCart(store.extrasSeleccionados); mover_carrito(); }} data-bs-dismiss="modal">Agregar e ir a pagar</button>
+                                    </div>
+                                    {/* </div> */}
                                 </div>
                             </div>
                         </div>
